a68b3f691919d349422a26f59ab77abd42b6395b
IvanKondakov <vany_kondakov@mail.ru>
2021-05-14 06:12:18 +0300

    sys

--- fortysixth/bot.py
+++ fortysixth/bot.py
@@ -1,10 +1,11 @@
 #import os
 import requests
+import sys
 
 def tel_bot():
     bot_api_key = "1802025375:AAH1e82nx7Elurv8XyExvF2YyngWPw9iKMs"
     channel_name = "@FortysixthBotDjango"
-    message = "Hello World"
+    message = sys.argv[1]
 
     url = f'https://api.telegram.org/bot{bot_api_key}/sendMessage'
 
